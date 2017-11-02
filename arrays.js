92e0368cbb2e1d4b52ed5a051292fb35a1857923
rosie w <rosie.wilt@gmail.com>
2017-11-02 01:46:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,5 +36,6 @@ function removeElementFromEndOfArray(array){
   console.log(array)
 }
 
-function accessElementInArray(array, index)
+function accessElementInArray(array, index) {
 array([index])
+}
