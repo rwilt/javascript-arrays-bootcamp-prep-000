453f20afa232d836df4737fc2ed66a5af9759a34
rosie w <rosie.wilt@gmail.com>
2017-11-02 02:00:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,18 +2,22 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   [element, ...array]
+  return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
   [...array, element]
+  return array
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
+  return array
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
