e17fff1a4152bcfbd1fd7786029c0ebfbb2b592a
rosie w <rosie.wilt@gmail.com>
2017-11-02 01:22:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,7 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ], 
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ],
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function addElementToBeginningOfArray(array, element) {
+  var array
+  var element
+  return [element, ...array]
+}
