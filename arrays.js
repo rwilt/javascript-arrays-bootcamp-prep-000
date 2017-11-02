724ed858f7fac9b7beecfe4108a47cf63c55d499
rosie w <rosie.wilt@gmail.com>
2017-11-02 01:45:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   console.log(array)
 }
+
+function removeElementFromEndOfArray(array){
+  array.slice(0,array.length -1)
+}
