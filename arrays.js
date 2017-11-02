5c5fcf9030362bb609015969ee2c079464b7a538
rosie w <rosie.wilt@gmail.com>
2017-11-02 01:29:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  var array = ["hello", "there"]
-  var element = "bye"
   [element, ...array]
-  return array
 }
