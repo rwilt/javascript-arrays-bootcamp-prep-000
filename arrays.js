f36ef92763d899e4d96f1a165eae591b2462041d
rosie w <rosie.wilt@gmail.com>
2017-11-02 01:30:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function addElementToEndOfArray(array, element) {
   [...array, element]
 }
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+}
