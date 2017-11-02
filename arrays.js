c0b4aa31341f65ce1c08f566db08d5fd960843a9
rosie w <rosie.wilt@gmail.com>
2017-11-02 01:29:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
   [element, ...array]
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+}
